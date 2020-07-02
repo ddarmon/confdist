6ea2dedba59f0b95af9d308ae9d3f47ba01ceb7a
David Darmon <gendarmon@gmail.com>
2020-07-02 15:52:37 -0400

    Adding a file to demo how to install a package from GitHub.

---
+++ installing-confdist-from-github.R
@@ -0,0 +1,11 @@
+## Installing for the first time:
+
+# install.packages('devtools')
+
+devtools::install_github("ddarmon/confdist")
+
+library(confdist2)
+
+## Updating the package
+
+devtools::update_packages('confdist2')
